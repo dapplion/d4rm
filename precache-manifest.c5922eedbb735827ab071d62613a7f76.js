acb5aea6c25b38419335d34acf4246fac8c7d345
event <event@e2r12p9.42.fr>
2019-03-09 22:23:16 +0100

    Updates

---
+++ precache-manifest.c5922eedbb735827ab071d62613a7f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9330a77a2f580ff9d2195c7ad0ed541c",
+    "url": "/decentralized-form/static/media/loader.9330a77a.gif"
+  },
+  {
+    "revision": "8998641c30b7bcedb93a",
+    "url": "/decentralized-form/static/js/runtime~main.b7643411.js"
+  },
+  {
+    "revision": "7401dcb00c7d124e27b1",
+    "url": "/decentralized-form/static/js/main.1b03a25d.chunk.js"
+  },
+  {
+    "revision": "6ab5ea11240af5a7bb8b",
+    "url": "/decentralized-form/static/js/2.b1c26f2d.chunk.js"
+  },
+  {
+    "revision": "7401dcb00c7d124e27b1",
+    "url": "/decentralized-form/static/css/main.7cd067e1.chunk.css"
+  },
+  {
+    "revision": "7be1030c51029d2d476c5c4ba3057fb0",
+    "url": "/decentralized-form/index.html"
+  }
+];
